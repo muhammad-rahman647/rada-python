bab11d6cf175c0fba3fe602bd2da65eaa0b05cbe
muhammad-rahman647 <rahmansheikh718@gmail.com>
2020-06-19 15:05:52 +0500

    final change hahahha

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -3,9 +3,12 @@ const {
    attendence,
    check
 } = require('../controllers/employeeController');
+const {
+   ensureAuthenticatedUser
+} = require('../config/auth');
 
 const router = express.Router();
 
-router.post('/:id/attendence', check, attendence);
+router.get('/:id/attendence', ensureAuthenticatedUser, check, attendence);
 
 module.exports = router;
